3f912748706e264f8620a685f0169d9d50c470f4
Chris Taylor <chris.taylor@fourth.com>
2015-07-09 11:24:16 -0100

    build job creates zip output file based on input parameters

--- Build/integration_package.ps1
+++ Build/integration_package.ps1
@@ -1,9 +1,14 @@
+Param(
+  [Parameter(mandatory=$true)][string]$componentname,
+  [Parameter(mandatory=$true)][string]$buildnumber
+)
+
 $currentPath = Split-Path $MyInvocation.MyCommand.Path
 $repository = Split-Path -Path $currentPath -Parent
 $ffd = Join-Path $repository 'FFDefinitionXML'
 $stylesheets = Join-Path $repository 'Stylesheets'
 $output = Join-Path $currentPath 'Output'
-$zipfilename = Join-Path $currentPath 'Output\HOSP-EU-Intgration.zip'
+$zipfilename = Join-Path $currentPath "Output\$componentname-$buildnumber.zip"
 
 if (test-path $zipfilename) {
 	rm $zipfilename 
